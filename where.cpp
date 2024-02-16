07c9726137006f17ff9e40086f417d430e8f6fdc
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-15 23:00:07 -0600

    Remove Yes No option

--- where.cpp
+++ where.cpp
@@ -26,23 +26,10 @@ int main()
     cout << "Input the string you want to find from: ";
     getline(cin, inputed2);
     
-    cout << "Are you looking for a character(Y) or a string(N): ";
-    cin >> yes_no;
-    cin.ignore();
-    
-    yes_no = toupper(yes_no);
-    if (yes_no =='N'){
-        string inputed1;
-        cout << "Input the substring you want to find: ";
-        getline(cin, inputed1);
-        findout(inputed1,inputed2);
-    } else {
-        char inputed1;
-        cout << "Input the charcter you want to find: ";
-        cin>>inputed1;
-        cin.ignore();
-        findout(inputed1,inputed2);
-    }
+    string inputed1;
+    cout << "Input the substring you want to find: ";
+    getline(cin, inputed1);
+    findout(inputed1,inputed2);
     
     return 0;
 }
